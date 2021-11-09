8370068a6d603059ab1d2091ff72dbd49b1fb4f5
snsepton <sonja.septon@utah.edu>
2021-11-08 22:13:31 -0700

    Functions

---
+++ index.js
@@ -0,0 +1,56 @@
+function sum(num1, num2){
+    return num1 + num2
+}
+
+var result = sum(10,5)
+console.log(result)
+
+function get_max(num1, num2, num3)
+{
+    var max = Math.max(10, 25, 35);
+    return max;
+}
+console.log(max)
+
+var max = [10, 3, 2];
+console.log(Math.max(...max));
+
+var number = 36;
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
+
+var str = " Learning this stuff is neato!"
+console.log(str.slice(14));
+
+var sum = 0;
+var count = 6;
+Fibnum = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+for (var i = 0; i < count; i++){
+    sum += Fibnum[i];
+}
+console.log(sum);
+
+var string = "monday night football";
+    counter = {};
+
+for (var i = 0, len = string.length; i < len; i += 1) {
+    counter[string[i]] = (counter[string[i]] || 0) + 1;
+}
+
+var biggest = -1, str;
+for (var key in counter) {
+    if (counter[key] > biggest) {
+        biggest = counter[key];
+        str = key;
+    }
+}
+
+console.log(str);
+
+
+
+
